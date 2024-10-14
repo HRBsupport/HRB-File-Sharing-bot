f86b8f69347d25313c2cb56b78dc3855ce7e8867
HRBsupport <wakeoneteam@gmail.com>
2024-10-14 09:15:52 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -226,7 +226,7 @@ async def start_command(client: Client, message: Message):
                 
             ]
                 )
-)
+
         await message.reply_text(
             text=START_MSG.format(
                 first=message.from_user.first_name,
