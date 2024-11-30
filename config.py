190293883b500e955c224cbaec7d22903e06ee20
HRBsupport <wakeoneteam@gmail.com>
2024-11-30 08:20:39 +0700

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ APP_ID = int(os.environ.get("APP_ID", "16024367"))
 #your api hash from https://my.telegram.org/apps
 API_HASH = os.environ.get("API_HASH", "e4d264f7d2608c48102500bff08f5b9c")
 #your channel_id from https://t.me/MissRose_bot by forwarding dummy message to rose and applying command `/id` in reply to that message
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001838940306"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002314378683"))
 #your id of telegram can be found by https://t.me/MissRose_bot with '/id' command
 OWNER_ID = int(os.environ.get("OWNER_ID", "7503448519"))
 #port set to default 8080
